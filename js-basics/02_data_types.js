29df372c01c224cdb234c77e8840a5502d60350a
Abdu Shahid <154607781+abdushahid@users.noreply.github.com>
2024-02-24 17:28:39 +0000

    Data type conversion

---
+++ js-basics/02_data_types.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as  newer version
+
+// alert();     alert statement can onlu be used in browser, in can't be use in node js.
+
+// Data types
+let name = "Shahid"; // string
+let age  = 18;  // number
+let isLoggedIn = true; // Boolean
+let temp = null // a stand alone value. it means empy and value is not assigned to variable
+
+// Number: range of number is 2 to the power of 53.
+// BigInt
+// String
+// Boollean
+// Null 
+// Undefing
+// Sybol: unique
+// object
+
+// Here is how to find a data type
+
+console.log(typeof name)
---
+++ js-basics/03_data type conversion.js
@@ -0,0 +1,48 @@
+let score = 33;
+let score1 = "33";
+let score2 = "33abc";
+let score3 = null;
+let score4 = undefined;
+let score5 = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+
+let valueInNumber1 = Number(score1);
+console.log(typeof(valueInNumber1));
+
+let valueInNumber2 = Number(score2);
+console.log(typeof(valueInNumber2));
+
+let valueInNumber3 = Number(score3);
+console.log(typeof(valueInNumber3));
+
+let valueInNumber4 = Number(score4);
+console.log(typeof(valueInNumber4));
+
+let valueInNumber5 = Number(score5);
+console.log(typeof(valueInNumber5));
+
+//  0 and 1 conversion
+let normal = 1;
+let Boolean1 = Boolean(normal);
+
+console.log(Boolean1)
+
+let normal1 = "abc";
+let Boolean2 = Boolean(normal1);
+
+console.log(Boolean2)
+
+// number to string conversion
+
+let aNumber = 44;
+let stringNumber = String(aNumber)
+console.log(typeof stringNumber)
