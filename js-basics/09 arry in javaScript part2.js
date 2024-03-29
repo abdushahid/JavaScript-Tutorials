701c95e8ce58436a8af93bb8696d83ba5f549cbd
Abdu Shahid <154607781+abdushahid@users.noreply.github.com>
2024-03-29 10:23:33 +0000

    array

--- js-basics/09 arry in javaScript part2.js
+++ js-basics/09 arry in javaScript part2.js
@@ -1,6 +1,7 @@
-const GD = ["Asad", "Shahab", "Adnan"]
-const WD = ["Shahid", "Arshad", "Hamza"]
+const GD = ["a", "b", "c"]
+const WD = ["d", "e", "f"]
 
 // GD.push(WD);
-GD.concat(WD);
 console.log(GD);
+const combinedArray = GD.concat(WD);
+console.log(combinedArray); // Output: ["a", "b", "c", "d", "e", "f"]
