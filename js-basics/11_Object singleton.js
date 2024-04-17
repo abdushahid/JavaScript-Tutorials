bdbee70ccf29f107e26c73cdbdf0da918da83de3
Abdu Shahid <154607781+abdushahid@users.noreply.github.com>
2024-04-17 17:04:32 +0000

    Objects part 2

---
+++ js-basics/11_Object singleton.js
@@ -0,0 +1,42 @@
+// Object part 2
+const fbUser = {}
+fbUser.id = "12423";
+fbUser.name = "Shahid";
+fbUser.isLoggedIn = false;
+
+// console.log(fbUser)
+
+
+// Nested objects
+
+const regularUser = {
+    email: "abc123@gmail.com",
+    fullName: {
+        firstName: "Abdu",
+        lastName: "Shahid"
+    }
+}
+// console.log(regularUser)
+// console.log(regularUser.fullName.lastName)
+
+
+//combining objects
+const obj1 = {1: "a", 2:"b"}
+const obj2 = {3: "c", 4:"d"}
+
+const obj4 = Object.assign(obj1, obj2)
+const obj3 = Object.assign({}, obj1, obj2) // {} is target and objects are sources
+
+// console.log(obj4);
+// console.log(obj3);
+
+const obj5 = {...obj1, ...obj2}
+// console.log(obj5);
+
+
+// Find all keys and values of an object
+console.log(fbUser)
+console.log(Object.keys(fbUser));
+console.log(Object.values(fbUser));
+console.log(Object.entries(fbUser));  // convert each key-value pair to arry
+console.log(fbUser.hasOwnProperty("isLoggedIn"));
