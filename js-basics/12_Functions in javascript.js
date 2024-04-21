5288287ae530e56b32bfa9d8048746c8403bca18
Abdu Shahid <154607781+abdushahid@users.noreply.github.com>
2024-04-21 07:22:34 +0000

    dealing with object and array in function

--- js-basics/12_Functions in javascript.js
+++ js-basics/12_Functions in javascript.js
@@ -26,7 +26,7 @@ function addTwoNumbers(number1, number2){
 
 const result = addTwoNumbers(3, 5)
 
-console.log("Result: ", result);
+// console.log("Result: ", result);
 
 
 function loginUserMessage(username = "sam"){ // Default value is "Sam"
@@ -38,40 +38,38 @@ function loginUserMessage(username = "sam"){ // Default value is "Sam"
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage())
-console.log(loginUserMessage("hitesh"))
-
+// console.log(loginUserMessage())
+// console.log(loginUserMessage("hitesh"))
 
 // function calculateCartPrice(val1, val2, ...num1){
-//     return num1
-// }
+function calculateCartPrice(...num1){  // ... REST AND SPREAD OPERATOR and convert numbers in function to array
+    return num1
+}
 
-// console.log(calculateCartPrice(200, 400, 500, 2000))
+console.log(calculateCartPrice(200, 400, 500, 2000))
 
-// const user = {
-//     username: "hitesh",
-//     prices: 199
-// }
+const user = {
+    username: "Shahid",
+    price: 199
+}
 
-// function handleObject(anyobject){
-//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
-// }
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
 
-// handleObject(user)
-// handleObject({
-//     username: "sam",
-//     price: 399
-// })
+handleObject(user)
+//YOU can directly pass an object in the function as well
+handleObject({
+    username: "sam",
+    price: 399
+})
 
+// Handling Arrays in function
 // const myNewArray = [200, 400, 100, 600]
 
-// function returnSecondValue(getArray){
-//     return getArray[1]
+// function returnValue(getArray){
+//     return getArray[1] // You should pass the argument
 // }
 
 // console.log(returnSecondValue(myNewArray));
-// console.log(returnSecondValue([200, 400, 500, 1000]));
-
-// function Shahid(){
-//     console.log("Shahid Khan")
-// }
+// console.log(returnSecondValue([200, 400, 500, 1000])); // Passing array directly
